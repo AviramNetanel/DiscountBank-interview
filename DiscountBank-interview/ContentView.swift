4e322134d81c7a558a1f4e7d47a78461ae380a0a
AviramNetanel <aviramnetanel@gmail.com>
2026-05-13 16:19:42 +0300

    Initial Commit

---
+++ DiscountBank-interview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiscountBank-interview
+//
+//  Created by Aviram Netanel on 13/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiscountBank-interview/DiscountBank_interviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscountBank_interviewApp.swift
+//  DiscountBank-interview
+//
+//  Created by Aviram Netanel on 13/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct DiscountBank_interviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
